96b55b98a4e4e09c4637c230e03446b8f6f1bd4e
kplakosh <kplakosh@gmail.com>
2018-11-26 21:36:43 -0800

    pop-up alert added for search field

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ searchButton.addEventListener('click', searchWeather);
 
 function searchWeather() {
     var cityName = (searchCity.value);
+    if (cityName.trim().length == 0) {
+        return alert('Please enter a City Name');
+    }
     var http = new XMLHttpRequest();
     var apikey = 'a2cbdd66c6fb90a2b9847ae10a3f22e7';
     var url = 'api.openweathermap.org/data/2.5/weather?q='
