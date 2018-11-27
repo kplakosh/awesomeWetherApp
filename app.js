24e7bfba98ba55ec72cd235b9b23851412f8e059
kplakosh <kplakosh@gmail.com>
2018-11-26 22:55:54 -0800

    print weather info in consol

--- app.js
+++ app.js
@@ -8,16 +8,20 @@ function searchWeather() {
         return alert('Please enter a City Name');
     }
     var http = new XMLHttpRequest();
-    var apikey = 'a2cbdd66c6fb90a2b9847ae10a3f22e7';
-    var url = 'api.openweathermap.org/data/2.5/weather?q=' + cityName;
+    var apiKey = 'a2cbdd66c6fb90a2b9847ae10a3f22e7';
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&appid=' + apiKey;
     var method = 'GET';
 
     http.open(method, url);
     http.onreadystatechange = function() {
         if (http.readyState == XMLHttpRequest.DONE && http.status === 200) {
             var data = JSON.parse(http.responseText);
-        } else if (http.readyState === XMLHttpRequest.Done) {
+            var weatherData = new Weather(cityName, data.weather[0].description.toUpperCase());
+            weatherData.temperature = data.main.temp;
+            console.log(weatherData);
+        } else if (http.readyState === XMLHttpRequest.DONE) {
             alert('Something Went Wrong!');
         }
     };
+    http.send();
 }
