b1771b46d7164dc9a721ebb1885c718365e7eaf8
kplakosh <kplakosh@gmail.com>
2018-11-26 21:57:32 -0800

    created state change

--- app.js
+++ app.js
@@ -10,4 +10,14 @@ function searchWeather() {
     var http = new XMLHttpRequest();
     var apikey = 'a2cbdd66c6fb90a2b9847ae10a3f22e7';
     var url = 'api.openweathermap.org/data/2.5/weather?q=' + cityName;
+    var method = 'GET';
+
+    http.open(method, url);
+    http.onreadystatechange = function() {
+        if (http.readyState == XMLHttpRequest.DONE && http.status === 200) {
+            var data = JSON.parse(http.responseText);
+        } else if (http.readyState === XMLHttpRequest.Done) {
+            alert('Something Went Wrong!');
+        }
+    };
 }
